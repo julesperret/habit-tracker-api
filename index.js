6c17b88d8b23a89fcb9d6ffb4f94c921022d9b26
julesperret <30316988+julesperret@users.noreply.github.com>
2024-01-25 19:15:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect("mongodb+srv://jules:Zrr23s5xDjdL6Ex6@cluster0.36joumz.mongodb.net/")
+  .connect("mongodb+srv://sujan:sujan@cluster0.yr7yqzq.mongodb.net/")
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -25,7 +25,7 @@ app.listen(port, () => {
 
 const Habit = require("./models/habit");
 
-//add habit
+//endpoint to create a habit in the backend
 app.post("/habits", async (req, res) => {
   try {
     const { title, color, repeatMode, reminder } = req.body;
@@ -41,5 +41,48 @@ app.post("/habits", async (req, res) => {
     res.status(200).json(savedHabit);
   } catch (error) {
     res.status(500).json({ error: "Network error" });
+  }
+});
+
+app.get("/habitslist", async (req, res) => {
+  try {
+    const allHabits = await Habit.find({});
+
+    res.status(200).json(allHabits);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/habits/:habitId/completed", async (req, res) => {
+  const habitId = req.params.habitId;
+  const updatedCompletion = req.body.completed; // The updated completion object
+
+  try {
+    const updatedHabit = await Habit.findByIdAndUpdate(
+      habitId,
+      { completed: updatedCompletion },
+      { new: true }
+    );
+
+    if (!updatedHabit) {
+      return res.status(404).json({ error: "Habit not found" });
+    }
+
+    return res.status(200).json(updatedHabit);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/habits/:habitId", async (req, res) => {
+  try {
+    const { habitId } = req.params;
+
+    await Habit.findByIdAndDelete(habitId);
+
+    res.status(200).json({ message: "Habit deleted succusfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to delete the habit" });
   }
 });
